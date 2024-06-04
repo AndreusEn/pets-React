647e1d61d7f45f7ccba122f07ad460e2ae094eff
Andreus <Andreusramirez1@gmail.com>
2024-06-04 17:46:00 -0600

    Modulo 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
+import Page404 from './pages/ERROR404';
+import Header from './Components/Header';
+
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 function App() {
 
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Sobre' element={<Sobre/>}/>
+        <Route path='*' element={<Page404/>} />
       </Routes>
     </Router>
   );
